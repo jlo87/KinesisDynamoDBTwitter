0645c4425bba36cd7215184d235d81b88e960b62
Jonathan Lo <64229972+jlo87@users.noreply.github.com>
2020-06-20 01:05:45 -0400

    Update twitterCreds.py

--- Producer/twitterCreds.py
+++ Producer/twitterCreds.py
@@ -1,6 +1,6 @@
 # Twtter credentials for Kinesis, DynamoDB and Twtter Project.
 
-consumer_key = 'U2T4Swl39Zad1VQwvVqpB1qTD'
-consumer_secret = '42gxpuBlU1O0IzDLTnem4p4C9dJJ8sF0fSESrbNfilwu9aNC1c'
-access_token_key = '254270825-XsdkhJF0Q5hRtvEsT8gMbQtPuA2SstVkBqMucLH5'
-access_token_secret = '2lTfuW50c4vf9ZzyvK6BUn0zJPIj17iUeKznVxv6dTpAy'
+consumer_key = "XXXxxXXX"
+consumer_secret = "xxXXxXX"
+access_token_key = "XXX-XXxxXXX"
+access_token_secret = "XXxxXXX"
