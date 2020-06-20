faa48b26c1d8eba72543f621c612535176d46237
Jonathan Lo <jonlo1987@gmail.com>
2020-06-19 23:25:12 -0400

    Create twitterKinesisBulk.py

---
+++ Producer/twitterKinesisBulk.py
@@ -0,0 +1,31 @@
+# Python code to use twitter api and feed data into Kinesis.
+# This minimizes the amount of writes we do into Kinesis.
+# Bulk write for each 100 records using put_records.
+
+from TwitterAPI import TwitterAPI
+import boto3
+import json
+import twitterCreds
+
+# Twitter credentials.
+
+consumer_key = twitterCreds.consumer_key
+consumer_secret = twitterCreds.consumer_secret
+access_token_key = twitterCreds.access_token_key
+access_token_secret = twitterCreds.access_token_secret
+
+api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
+
+kinesis = boto3.client('kinesis')
+
+r = api.request('statuses/filter', {'locations':'-90,-90,90,90'})
+tweets = []
+count = 0
+for item in r:
+	jsonItem = json.dumps(item)
+	tweets.append({'Data':jsonItem, 'PartitionKey':"filler"})
+	count += 1
+	if count == 100:
+		kinesis.put_records(StreamName="twitter", Records=tweets)
+		count = 0
+		tweets = []
